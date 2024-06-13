f688494bafb5de97f2ee6b62b2e37e485b9c8dac
Iamasink <iamasink@proton.me>
2024-06-13 10:23:18 +0100

    commit script

--- commit.bat
+++ commit.bat
@@ -1,4 +1,8 @@
 @REM setup and commit
-cd maps\
+cd maps
 dmxconvert -i de_nuke_custom.vmap -o de_nuke_custom.vmap-txt -oe keyvalues2 -of vmap
 dmxconvert -i de_nuke_custom_prefab.vmap -o de_nuke_custom_prefab.vmap-txt -oe keyvalues2 -of vmap
+git add .
+SET /P msg=Commit message:
+git commit -m "%msg%"
+timeout /t 5
