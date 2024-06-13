12a4b4e84dc53fac5636561fc9419d66636d728c
Iamasink <iamasink@proton.me>
2024-06-13 10:18:03 +0100

    initial commit

---
+++ commit.bat
@@ -0,0 +1,4 @@
+@REM setup and commit
+cd maps\
+dmxconvert -i de_nuke_custom.vmap -o de_nuke_custom.vmap-txt -oe keyvalues2 -of vmap
+dmxconvert -i de_nuke_custom_prefab.vmap -o de_nuke_custom_prefab.vmap-txt -oe keyvalues2 -of vmap
