e8640085c32d6684b9eb60823c2908493c1ea163
Iamasink <iamasink@proton.me>
2024-06-13 11:18:49 +0100

    set map parameters

--- commit.bat
+++ commit.bat
@@ -1,8 +1,9 @@
 @REM setup and commit
+SET /P msg=Commit message:
 cd maps
 dmxconvert -i de_nuke_custom.vmap -o de_nuke_custom.vmap-txt -oe keyvalues2 -of vmap
 dmxconvert -i de_nuke_custom_prefab.vmap -o de_nuke_custom_prefab.vmap-txt -oe keyvalues2 -of vmap
+cd ..
 git add .
-SET /P msg=Commit message:
 git commit -m "%msg%"
 timeout /t 5
