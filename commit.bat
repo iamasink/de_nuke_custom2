23662a8914ac45673959a58c674b9b3bb754f0c2
Iamasink <iamasink@proton.me>
2024-06-19 08:51:12 +0100

    stuff idk

--- commit.bat
+++
@@ -1,9 +0,0 @@
-@REM setup and commit
-SET /P msg=Commit message:
-cd maps
-dmxconvert -i de_nuke_custom.vmap -o de_nuke_custom.vmap-txt -oe keyvalues2 -of vmap
-dmxconvert -i de_nuke_custom_prefab.vmap -o de_nuke_custom_prefab.vmap-txt -oe keyvalues2 -of vmap
-cd ..
-git add .
-git commit -m "%msg%"
-timeout /t 5
